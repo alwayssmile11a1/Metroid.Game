1c964e9325aac1df45d59a0e765ecdc61c6a6771
Trương Ngọc Sơn <alwayssmile11a1@outlook.com>
2017-09-25 21:53:10 +0700

    add a small comment (nothing much to worry about)

--- Metroid/Metroid/stdafx.h
+++ Metroid/Metroid/stdafx.h
@@ -1,7 +1,7 @@
 // stdafx.h : include file for standard system include files,
 // or project specific include files that are used frequently, but
 // are changed infrequently
-//
+// this headed is built only one time, thus will reduce the time we have to build the include list over and over again
 
 #pragma once
 
